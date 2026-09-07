76c3dfdecf07dd5202862c99d6fdb60cdb63664f
CTSLLC-png <determinedman42@gmail.com>
2026-09-07 07:31:12 -0400

    Trigger production deploy with live ONE SCHENECTADY endpoint

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+// Production configuration for ONE SCHENECTADY
 window.ONE_SCHENECTADY_CONFIG={
   DATA_ENDPOINT:'https://script.google.com/macros/s/AKfycbzooZrV11LSDK1MjDQiXhYmjmrduUUochp09WUBVeGb0tD5LhmQOLbZg1RIKJPeKZ_d-Q/exec',
   MIN_PUBLIC_SAMPLE:25
